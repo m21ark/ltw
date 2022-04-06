da2e66aa18c50ba01f2ca21106bbb4d085a90f6f
Marco <marco14lyoko@gmail.com>
2022-04-06 19:17:59 +0100

    Base Structure

---
+++ article.php
@@ -0,0 +1,12 @@
+<?php require_once("templates/common_tpt.php");?>
+
+<html lang="en">
+	<?php output_head();?>
+<body>
+	<?php output_header();?>
+ 	<?php output_head();?>
+	<?php output_main();?>
+	<a href="index.php">Return</a>
+	<?php output_footer();?>
+</body>
+</html>
---
+++ templates/common_tpt.php
@@ -0,0 +1,47 @@
+<?php function output_head(){?>
+
+	 <head>
+		<meta charset="UTF-8">
+		<title>Restaurant</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+
+<?php }?>
+
+
+
+<?php function output_header(){?>
+
+    <header>
+        <h1>My website</h1>
+        <p>Welcome to my website!</p>
+    </header>
+
+<?php }?>
+
+
+
+<?php function output_main(){?>
+
+    <main>
+        <p>Pizza!</p>
+        <img src="docs/pizza.jpg" alt="Pizza">
+    </main>
+
+<?php }?>
+
+
+
+<?php function output_footer(){?>
+
+    <footer>
+        <p>Spicy Restaurant</p>
+    </footer>
+
+<?php }?>
+
+
+
+
+
+
