af60898e1b2e43cd2b9faddc5d8834c0b57f402f
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2022-04-19 10:07:02 +0100

    sql script changes

--- database/DBscript.sql
+++ database/DBscript.sql
@@ -1,77 +1,97 @@
-DROP TABLE Restaurant;
-DROP TABLE Dish;
-DROP TABLE "Order";
-DROP TABLE OrderState;
-DROP TABLE Menu;
-DROP TABLE Review;
-DROP TABLE Photo;
-DROP TABLE Customer;
-DROP TABLE Owner;
-DROP TABLE DishOrder;
-DROP TABLE CustomerDish;
-DROP TABLE Favorite;
+DROP TABLE IF EXISTS Restaurant;
+DROP TABLE IF EXISTS Dish;
+DROP TABLE IF EXISTS "Order";
+DROP TABLE IF EXISTS OrderState;
+DROP TABLE IF EXISTS Menu;
+DROP TABLE IF EXISTS Review;
+DROP TABLE IF EXISTS Photo;
+DROP TABLE IF EXISTS Customer;
+DROP TABLE IF EXISTS Owner;
+DROP TABLE IF EXISTS DishOrder;
+DROP TABLE IF EXISTS CustomerFavoriteDishes;
+DROP TABLE IF EXISTS CustomerFavoriteRestaurants;
+DROP TABLE IF EXISTS User;
 
 CREATE TABLE Restaurant (
-	RestaurantID INT PRIMARY KEY NOT NULL,
-	Name VARCHAR(25) NOT NULL,
-	Address VARCHAR(25) NOT NULL,
-	Category VARCHAR(25) NOT NULL);
+	RestaurantID INTEGER PRIMARY KEY,
+	Name VARCHAR NOT NULL,
+	Address VARCHAR NOT NULL,
+	Category VARCHAR );
 	
 CREATE TABLE Dish (
-	DishID INT PRIMARY KEY NOT NULL,
-	Name VARCHAR(25) NOT NULL,
-	Price VARCHAR(25) NOT NULL,
-	Category VARCHAR(25) NOT NULL);
+	DishID INTEGER PRIMARY KEY,
+	Name VARCHAR NOT NULL,
+	Price VARCHAR NOT NULL,
+	Category VARCHAR);
 	
 CREATE TABLE OrderState (
-	OrderStateID INT PRIMARY KEY NOT NULL,
-	StateName VARCHAR(25) NOT NULL);
+	OrderStateID INTEGER PRIMARY KEY,
+	StateName VARCHAR NOT NULL);
 	
 CREATE TABLE Customer (
-	CustomerID INT PRIMARY KEY NOT NULL);
+	CustomerID INT PRIMARY KEY,
+	FOREIGN KEY (CustomerID) REFERENCES User(CustomerID));
 	
-CREATE TABLE "Order" (
-	OrderID INT PRIMARY KEY NOT NULL,
-	OrderStateID INT,
-	CustomerID INT,
+CREATE TABLE "Order" ( -- FALTAVA O RESTAURANTE
+	OrderID INTEGER PRIMARY KEY,
+	OrderStateID INTEGER,
+	CustomerID INTEGER,
+	RestaurantID INTEGER,
+	FOREIGN KEY (RestaurantID) REFERENCES Restaurant(RestaurantID),
 	FOREIGN KEY (OrderStateID) REFERENCES OrderState(OrderStateID),
 	FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID));
 	
-CREATE TABLE Menu (
-	MenuID INT PRIMARY KEY NOT NULL,
-	DishID INT,
-	FOREIGN KEY (DishID) REFERENCES Dish(DishID));
+CREATE TABLE Menu ( -- Não é só um dish --> assim já dará
+	RestaurantID INTEGER,
+	DishID INTEGER,
+	FOREIGN KEY (RestaurantID) REFERENCES Restaurant(RestaurantID),
+ 	FOREIGN KEY (DishID) REFERENCES Dish(DishID),
+	PRIMARY KEY (RestaurantID, DishID));
 	
-CREATE TABLE Review (
-	ReviewID INT PRIMARY KEY NOT NULL,
-	Score INT,
-	ReviewComment VARCHAR(256),
-	DateOfReview DATE);
+CREATE TABLE Review ( --- FALTA LIGAR COM O RESTAURANTE
+	ReviewID INTEGER PRIMARY KEY,
+	Score INTEGER,
+	ReviewComment VARCHAR NOT NULL,
+	DateOfReview INTEGER NOT NULL,  --- insert the date in epoch format
+	RestaurantID INTEGER NOT NULL,
+	FOREIGN KEY (RestaurantID) REFERENCES Restaurant(RestaurantID)
+);
 	
-CREATE TABLE Photo (
-	PhotoID INT PRIMARY KEY NOT NULL,
-	URL VARCHAR(256) NOT NULL);
+CREATE TABLE Photo ( --- Não será assim o formato (com URL) deverá ser um BLOB
+	PhotoID Dishes PRIMARY KEY,
+	URL VARCHAR NOT NULL);
 	
-CREATE TABLE Owner (
-	OwnerID INT PRIMARY KEY NOT NULL);
+CREATE TABLE Owner ( --- Faltava LIGAR COM RESTAURANTE e Ligar com User
+	OwnerID INTEGER,
+	RestaurantID INTEGER,
+	FOREIGN KEY (RestaurantID) REFERENCES Restaurant(RestaurantID),
+	FOREIGN KEY (OwnerID) REFERENCES User(OwnerID),
+	PRIMARY KEY (OwnerID, RestaurantID)
+	);
 	
-CREATE TABLE DishOrder (
-	DishID INT,
-	OrderID INT,
+CREATE TABLE DishOrder ( --- SE FIZERMOS ORDER DE 2 PRATOS IGUAIS - SERÁ QUE RESULTA? Parece que sim, mas confirmar ao povoar
+	DishID INTEGER,
+	OrderID INTEGER,
 	FOREIGN KEY (DishID) REFERENCES Dish(DishID),
 	FOREIGN KEY (OrderID) REFERENCES "Order"(OrderID));
 	
-CREATE TABLE CustomerDish (
-	CustomerID INT,
-	DishID INT,
+CREATE TABLE CustomerFavoriteDishes ( 
+	CustomerID INTEGER,
+	DishID INTEGER,
 	FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID),
 	FOREIGN KEY (DishID) REFERENCES Dish(DishID));
 	
-CREATE TABLE Favorite (
-	RestaurantID INT,
-	CustomerID INT, 
+CREATE TABLE CustomerFavoriteRestaurants (
+	CustomerID INTEGER,
+	RestaurantID INTEGER, 
 	FOREIGN KEY (RestaurantID) REFERENCES Restaurant(RestaurantID),
 	FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID));
 	
-	
+CREATE TABLE User ( --- adicionar foto
+	UserId INTEGER PRIMARY KEY,
+	username VARCHAR,
+	password VARCHAR,
+	Address VARCHAR,
+	phoneNumer VARCHAR
+)
 	
--- database/connection.php
+++ database/connection.php
@@ -2,7 +2,7 @@
 
 function getDatabaseConnection(){
 
-    $db = new PDO('sqlite:database/news.db');
+    $db = new PDO('sqlite:database/database.db');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
