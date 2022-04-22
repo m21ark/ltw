c43508c56a2869ceec1233a6038b26cddca5e41f
fosb <fosbtank@gmail.com>
2022-04-22 17:06:15 +0100

    fixed Customer table, foreign key pointed to itself and not the User table. started mock data

---
+++ database/DBmockdata.sql
@@ -0,0 +1,24 @@
+INSERT INTO Restaurant VALUES (1, "Casa da Mãe Joana", "Rua da Porta Torta nº 62", "Sushi");
+INSERT INTO Restaurant VALUES (2, "Restaurante Avenida", "Avenida da Luz nº 3", "Comida Portuguesa");
+INSERT INTO Restaurant VALUES (3, "Bordoaria", "Rua da Catedral nº15", "Comida Portuguesa");
+INSERT INTO Restaurant VALUES (4, "Canto do Ananás", "Avenida Central nº45", "Comida Italiana");
+INSERT INTO Restaurant VALUES (5, "Rei dos Hamburguers", "Rua do Canto n6", "Hamburgaria");
+
+INSERT INTO Dish VALUES (1, "Francesinha", "8.45", "Comida Portuguesa");
+INSERT INTO Dish VALUES (2, "Rojões à Portuguesa", "7.60", "Comida Portuguesa");
+INSERT INTO Dish VALUES (3, "Feijoada à moda do Porto", "8.20", "Comida Portuguesa");
+--adivinhem quem é que não gosta de sushi
+INSERT INTO Dish VALUES (4, "Disgusting Raw Fish #1", "-20", "Sushi");
+INSERT INTO Dish VALUES (5, "Disgusting Raw Fish #2", "-20", "Sushi");
+INSERT INTO Dish VALUES (6, "Disgusting Raw Fish #3", "-20", "Sushi");
+INSERT INTO Dish VALUES (7, "Pizza Hawaina", "6.7", "Comida Italiana");
+INSERT INTO Dish VALUES (8, "Massa Carbonara", "7.6", "Comida Italiana");
+INSERT INTO Dish VALUES (9, "Lasanha Vegetariana" , "-10", "Comida Italiana");
+INSERT INTO Dish VALUES (10, "Cheeseburger", "5.5", "Hamburgaria");
+INSERT INTO Dish VALUES (11, "Baconburger", "6", "Hamburgaria");
+INSERT INTO Dish Values (12, "BurguerBurguer", "10", "Hamburgaria");
+
+INSERT INTO OrderState VALUES (1, "Received");
+INSERT INTO OrderState VALUES (2, "Preparing");
+INSERT INTO OrderState VALUES (3, "Delivered");
+INSERT INTO OrderState VALUES (4, "Ready");
