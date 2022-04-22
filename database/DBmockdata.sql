568f0baa70b12ff500a2afdb3e40d691a2c0a57a
fosb <fosbtank@gmail.com>
2022-04-22 17:46:11 +0100

    first draft of mock data

--- database/DBmockdata.sql
+++ database/DBmockdata.sql
@@ -11,6 +11,7 @@ INSERT INTO Dish VALUES (3, "Feijoada à moda do Porto", "8.20", "Comida Portugu
 INSERT INTO Dish VALUES (4, "Disgusting Raw Fish #1", "-20", "Sushi");
 INSERT INTO Dish VALUES (5, "Disgusting Raw Fish #2", "-20", "Sushi");
 INSERT INTO Dish VALUES (6, "Disgusting Raw Fish #3", "-20", "Sushi");
+--adivinhem quem é que não gosta de sushi
 INSERT INTO Dish VALUES (7, "Pizza Hawaina", "6.7", "Comida Italiana");
 INSERT INTO Dish VALUES (8, "Massa Carbonara", "7.6", "Comida Italiana");
 INSERT INTO Dish VALUES (9, "Lasanha Vegetariana" , "-10", "Comida Italiana");
@@ -22,3 +23,49 @@ INSERT INTO OrderState VALUES (1, "Received");
 INSERT INTO OrderState VALUES (2, "Preparing");
 INSERT INTO OrderState VALUES (3, "Delivered");
 INSERT INTO OrderState VALUES (4, "Ready");
+
+-- all passwords are batata
+INSERT INTO "User" VALUES (1, "ASTRAZENECA@COVID.CN", "covid was an inside job", "8467b174e821587c4a0545fd8e57204a398c66d4", "nah im just kidding im bored making mock data is the most boring shit ever", "914442233");
+INSERT INTO "User" VALUES (2, "1337gamer123@gaymingworld.com", "1337eater", "8467b174e821587c4a0545fd8e57204a398c66d4", "Rua da Quinta nº 4", "913325436");
+INSERT INTO "User" VALUES (3, "amtrap@yahoo.com", "AnaMaria", "8467b174e821587c4a0545fd8e57204a398c66d4", "Avenida Central nº 6", "914531122");
+INSERT INTO "User" VALUES (4, "jjmigue@gmail.com", "JotaJota", "8467b174e821587c4a0545fd8e57204a398c66d4!*", "Beco dos becos nº 5", "934531123");
+INSERT INTO "User" VALUES (5, "imanalbatroz@burb.com", "ImABirdNotAPerson", "8467b174e821587c4a0545fd8e57204a398c66d4", "The Open Sea", "931124312");
+INSERT INTO "User" VALUES (6, "analuiza123@iol.pt", "Ana", "8467b174e821587c4a0545fd8e57204a398c66d4", "Rua da Porta nº 54", "923421123");
+
+INSERT INTO Customer VALUES (1, 1);
+INSERT INTO Customer VALUES (2, 2);
+INSERT INTO Customer VALUES (3, 3);
+INSERT INTO Customer VALUES (4, 4);
+INSERT INTO Customer VALUES (5, 5);
+
+INSERT INTO "Order" VALUES (1, 2, 3, 4);
+INSERT INTO "Order" VALUES (2, 4, 2, 3);
+INSERT INTO "Order" VALUES (3, 1, 3, 5);
+INSERT INTO "Order" VALUES (4, 2, 4, 5);
+INSERT INTO "Order" VALUES (5, 3, 2, 1);
+
+INSERT INTO Menu VALUES (1, 1
+
+INSERT INTO Review VALUES (1, "85", "Good food, great service", "2022-01-09", 1);
+INSERT INTO Review VALUES (2, "75", "Had a great time", "2022-02-19", 2);
+INSERT INTO Review VALUES (3, "65", "Not the best service, but the food was good", "2022-01-22", 3);
+INSERT INTO Review VALUES (4, "25", "Food was cold and staff was rude", "2022-03-01", 4);
+INSERT INTO Review VALUES (5, "93", "Best restaurant ever", "2022-02-10", 5);
+
+INSERT INTO Owner VALUES (1, 1);
+INSERT INTO Owner VALUES (2, 2);
+INSERT INTO Owner VALUES (3, 3);
+INSERT INTO Owner VALUES (4, 4);
+INSERT INTO Owner VALUES (5, 5);
+
+INSERT INTO DishOrder VALUES (1, 1);
+INSERT INTO DishOrder VALUES (2, 2);
+INSERT INTO DishOrder VALUES (3, 3);
+INSERT INTO DishOrder VALUES (4, 4);
+INSERT INTO DishOrder VALUES (5, 5);
+
+INSERT INTO CustomerFavoriteDishes VALUES (1, 1);
+INSERT INTO CustomerFavoriteDishes VALUES (2, 2);
+INSERT INTO CustomerFavoriteDishes VALUES (3, 3);
+INSERT INTO CustomerFavoriteDishes VALUES (4, 4);
+INSERT INTO CustomerFavoriteDishes VALUES (5, 5);
