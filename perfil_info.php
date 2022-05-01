46d7900008e5dfa6330356726d523c031eb4067e
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2022-05-01 23:30:06 +0100

    fixing bug and adding favorite connection to db

--- perfil_info.php
+++ perfil_info.php
@@ -1,14 +1,42 @@
 <?php
 
+declare(strict_types = 1);
+
+require_once(__DIR__ . "/database/Users/user_composite.class.php");
+require_once(__DIR__ . "/database/Users/customer.class.php");
+
+session_start();
+
+if ($_SESSION['user'] == null) die(header('Location: /')); // TODO: MENSSAGE ERROR OR SOMETHING 
+
+$user = unserialize($_SESSION['user']);
+
+$customer = $user->hasPermission("Customer");
+if ($customer === null) die(header('Location: /'));
+
 require_once("templates/common.tpt.php");
 require_once("templates/plates_carrossel.tpt.php");
 require_once("templates/restaurants_carrossel.tpt.php");
 require_once("database/connection.php");
 
+
+
 $db = getDatabaseConnection();
 
-$dishes = Dish::getRandomDishes($db, 5);
-$restaurants = Restaurant::getRandomRestaurants($db, 5);
+$dishesID = $customer->getFavoriteDishes($db);
+$restaurantsID = $customer->getFavoriteRestaurants($db);
+
+$dishes = [];
+$restaurants = [];
+
+foreach ($restaurantsID as $id) {
+    array_push($restaurants, Restaurant::getRestaurant($db, (int)$id['RestaurantID']));
+}
+
+foreach ($dishesID as $id) {
+    array_push($dishes, Dish::getDish($db, (int)$id['DishID']));
+}
+
 
 output_header();
 drawUserFavCards($dishes, $restaurants);
