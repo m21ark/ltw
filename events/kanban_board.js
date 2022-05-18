46c2b66ba66d63ce0fdf395100c308f289af25f9
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2022-05-19 00:03:17 +0100

    working on css for kanban

--- events/kanban_board.js
+++ events/kanban_board.js
@@ -5,13 +5,10 @@ function dragItem() {
     items.forEach( element => {
 
         element.addEventListener("dragstart", function(ev) {
-            console.log("eeeee")
-            //element.dataTransfer = element.originalEvent.dataTransfer;
             ev.dataTransfer.setData("text/plain", element.getAttribute('data-id'));
         });
 
         element.addEventListener("drop", function(ev) {
-            console.log("ekokokoke")
             ev.preventDefault();   
         });
 
@@ -31,24 +28,20 @@ function createDropZone() {
         element.addEventListener("dragover", e => {
             e.preventDefault();
             element.classList.add("kanban__dropzone--active");
-            console.log("hregepplplg")
     
         });
     
         element.addEventListener("dragleave", () => {
-            element.classList.remove("kanban__dropzone--active");
-            console.log("hregekookog")
-            
+            element.classList.remove("kanban__dropzone--active");            
         });
     
         element.addEventListener("drop", e => {
-            console.log("hregeg")
     
             e.preventDefault();
             element.classList.remove("kanban__dropzone--active");
      
             const columnElement = element.closest(".kanban__column");
-            const columnId = Number(columnElement.dataset.id);
+            const columnId = Number(columnElement.dataset.id); // TO USE
             const dropZonesInColumn = Array.from(columnElement.querySelectorAll(".kanban__dropzone"));
             const droppedIndex = dropZonesInColumn.indexOf(element);
             const itemId = Number(e.dataTransfer.getData("text/plain"));
