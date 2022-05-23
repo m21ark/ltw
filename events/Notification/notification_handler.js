4211395ac5258c44df3f6aafa0b5a97d196c720f
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2022-05-23 14:12:52 +0100

    Notf. timeout

--- events/Notification/notification_handler.js
+++ events/Notification/notification_handler.js
@@ -1,20 +1,5 @@
 function notification() {
-    // const request = new XMLHttpRequest();
-    // request.withCredentials = true;
-    // request.open("GET", "events/Notification/test.php", true);
-    // request.setRequestHeader('Cache-Control', 'no-cache');
-    // request.timeout = 500;
-    // request.setRequestHeader('Content-Type',
-    //     'application/x-www-form-urlencoded')
-    // request.onreadystatechange = function (data) {
-    //     if (request.readyState == XMLHttpRequest.DONE) {
-    //         const new_notifications = this.responseText;
-    //         if (new_notifications.includes('New Order Pending')) {
-    //             let audio = new Audio('audio/notification.wav');
-    //             audio.play();
-    //         }
-    //     }
-    // }
+    
     const id = document.querySelector('.user_id');
     if (id === null)
         return;
@@ -37,6 +22,10 @@ function notification() {
             art.appendChild(p)
             myNotification.appendChild(art);
             document.querySelector('main').insertBefore(myNotification, document.querySelector('main').firstChild);
+
+            setTimeout(function() {
+                art.parentNode.removeChild(art);
+            }, 5000);
         }
     })    
 }
