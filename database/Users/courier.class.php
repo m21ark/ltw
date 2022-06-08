c1277c4a3b595fc9a8008f2b3eb98c6893f93993
Marco <marco14lyoko@gmail.com>
2022-06-08 23:46:43 +0100

    Thanks DB Classes

--- database/Users/courier.class.php
+++ database/Users/courier.class.php
@@ -11,27 +11,20 @@ class Courier extends User
     public static function login(PDO $db, string $email, string $password): ?User
     {
 
-        $stmt = $db->prepare('SELECT * FROM User WHERE lower(email) = ?');
-        $stmt->execute(array($email));
-
-        $user = $stmt->fetch();
-
-        if (!($user && password_verify($password, $user['password'])))
-            return null;
-
-
-        // ------------------------------------------------------------------
-
         $stmt = $db->prepare('
-            SELECT UserID, username, Address, phoneNumber, email
+            SELECT UserID, username, Address, phoneNumber, email, password
             FROM Courier LEFT JOIN USER on (CourierID = UserID)
             WHERE lower(email) = ?
         ');
 
         $stmt->execute(array(strtolower($email)));
+        $user = $stmt->fetch();
 
 
-        if ($customer = $stmt->fetch()) {
+        if (!($user && password_verify($password, $user['password'])))
+            return null;
+
+        if ($customer = $user) {
             return new Courier(
                 (int)$customer['UserId'],
                 $customer['username'],
--- database/Users/restaurant_owner.class.php
+++ database/Users/restaurant_owner.class.php
@@ -13,26 +13,20 @@ class RestaurantOwner extends User
     public static function login(PDO $db, string $email, string $password): ?User
     {
 
-        $stmt = $db->prepare('SELECT * FROM User WHERE lower(email) = ?');
-        $stmt->execute(array($email));
-
-        $user = $stmt->fetch();
-
-
-        if (!($user && password_verify($password, $user['password'])))
-            return null;
-
-        // ------------------------------------------------------------------
-
         $stmt = $db->prepare('
-            SELECT UserID, username, Address, phoneNumber, email
+            SELECT UserID, username, Address, phoneNumber, email, password
             FROM Owner LEFT JOIN USER on (OwnerID = UserID)
             WHERE lower(email) = ?
         ');
 
         $stmt->execute(array(strtolower($email)));
 
-        if ($customer = $stmt->fetch()) {
+        $user = $stmt->fetch();
+
+        if (!($user && password_verify($password, $user['password'])))
+            return null;
+
+        if ($customer = $user) {
             return new RestaurantOwner(
                 (int)$customer['UserId'],
                 $customer['username'],
