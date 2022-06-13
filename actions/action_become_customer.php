94fc8959d45ec7330e9e5ac9337219cdc9036bf4
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2022-06-13 22:43:32 +0100

    become now full working

--- actions/action_become_customer.php
+++ actions/action_become_customer.php
@@ -3,8 +3,26 @@
 declare(strict_types=1);
 
 require_once(__DIR__ . "/../database/connection.php");
+require_once(__DIR__ . "/../database/Users/user_composite.class.php");
 require_once(__DIR__ . "/../database/Users/customer.class.php");
+require_once(__DIR__ . '/../utils/session.php');
+
+$session = new Session();
+
+$session = new Session();
+if (!$session->isLoggedIn()) {
+    $session->addMessage('erro', 'Login required. Redirected to main page');
+    die(header('Location: /'));
+}
 
 $db = getDatabaseConnection();
 
+$costPriUser = unserialize($session->getUserSerialized());
+$user = $costPriUser->permissions[0];
+
+
+array_push($costPriUser->permissions, new Customer($user->id, $user->username, $user->address, $user->phone, $user->email));
+
 Customer::addCostumerById($db, (int)$_POST['id']);
+
+$session->setUser($costPriUser);
