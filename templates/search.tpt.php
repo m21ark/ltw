ce3e906df6a56724f8632ddb8ec6b01f399500ef
Marco <marco14lyoko@gmail.com>
2022-06-14 14:49:47 +0100

    Css changes

--- templates/search.tpt.php
+++ templates/search.tpt.php
@@ -8,8 +8,7 @@ function drawSearchBox()
     <section class="search" id="search_box">
         <form class="container search_box">
             <h2>Search</h2>
-            <input type="search" id="search_box_input" name="q" autofocus placeholder="Search" value=<?= htmlentities(($_GET['q']) ? $_GET['q'] : "") ?>>
-            <!-- <input type="submit" class="link_button" value="Search   &#128270;">  -->
+            <input type="search" id="search_box_input" name="q" placeholder="Search" value=<?= htmlentities(($_GET['q']) ? $_GET['q'] : "") ?>>
         </form>
     </section>
 
