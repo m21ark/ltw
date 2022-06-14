878545ded0f545eb9d9011282d9ecb6361c34595
Ricardo Matos <ricardo.andre.de.matos@gmail.com>
2022-06-14 14:40:00 +0100

    REST APIS response and Cleaning

--- apis/api_order.php
+++ apis/api_order.php
@@ -5,10 +5,7 @@ declare(strict_types=1);
 require_once(__DIR__ . "/../database/connection.php");
 require_once(__DIR__ . "/../database/restaurant.class.php");
 require_once(__DIR__ . "/../database/order.class.php");
-require_once(__DIR__ . '/../utils/session.php');
 require_once(__DIR__ . "/../templates/plates_carrossel.tpt.php");
-
-$session = new Session();
 
 if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
     $db = getDatabaseConnection();
@@ -45,3 +42,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
     die();
 }
+
+header("HTTP/1.1 400 Bad Request");
--- apis/api_plates.php
+++ apis/api_plates.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 require_once(__DIR__ . "/../database/connection.php");
 require_once(__DIR__ . "/../database/restaurant.class.php");
-require_once(__DIR__ . '/../utils/session.php');
 require_once(__DIR__ . "/../templates/plates_carrossel.tpt.php");
-
-$session = new Session();
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     header('Access-Control-Allow-Origin: *');
@@ -43,3 +40,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
     die();
 }
+
+header("HTTP/1.1 400 Bad Request");
